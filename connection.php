9b26e67543fead12719f9a1b224dd81d6f81f035
haley <misshaleyn@gmail.com>
2017-08-13 19:09:53 -0400

    Test!

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $password = "root";
 $dbname = "dad";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
